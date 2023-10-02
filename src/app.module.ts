3509572375342f47bfdb2111e5647908e8cd5de6
bohdan-mykhailenko <76702178+bohdan-mykhailenko@users.noreply.github.com>
2023-10-02 14:54:51 +0300

    implement serving static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseConfig } from './config/database.config';
 import { SuperheroesModule } from './modules/superheroes.module';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { SequelizeModule } from '@nestjs/sequelize';
+import * as path from 'path';
 
 @Module({
-  imports: [SequelizeModule.forRoot(databaseConfig), SuperheroesModule],
+  imports: [
+    SequelizeModule.forRoot(databaseConfig),
+    SuperheroesModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'public', 'images', 'superheroes'),
+      serveRoot: '/images/superheroes',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
