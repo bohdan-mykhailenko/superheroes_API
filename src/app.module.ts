1687977114ac3730f48dbe3e0940f9809daa0699
bohdan-mykhailenko <76702178+bohdan-mykhailenko@users.noreply.github.com>
2023-10-02 14:15:20 +0300

    create database model, migration and connect to server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './config/database.config';
+import { SuperheroesModule } from './modules/superheroes.module';
 
 @Module({
-  imports: [],
+  imports: [SequelizeModule.forRoot(databaseConfig), SuperheroesModule],
   controllers: [AppController],
   providers: [AppService],
 })
