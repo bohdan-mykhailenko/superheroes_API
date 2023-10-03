82192cd58822e09d57851fff59cd84990ce5feff
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2023-10-03 17:06:01 +0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,11 @@ import { SuperheroesModule } from './superheroes/superheroes.module';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { CorsModule } from '@nestjs/platform-express';
 import { databaseConfig } from './config/database.config';
 import * as path from 'path';
 
 @Module({
   imports: [
-    CorsModule,
     SequelizeModule.forRoot(databaseConfig),
     SuperheroesModule,
     ServeStaticModule.forRoot({
