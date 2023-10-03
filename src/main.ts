84e17e2f2664e9be5f0376827510cec6e283f68b
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2023-10-03 17:00:06 +0300

    update main.ts for cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,PATCH,POST,DELETE',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
+
+
   await app.listen(PORT);
 }
 bootstrap();
